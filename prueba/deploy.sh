39a204f793e466dd25fb6d0589ebe26c0b6a29ce
Your Name <you@example.com>
2025-03-14 20:59:23 +0100

    dd

--- prueba/deploy.sh
+++ prueba/deploy.sh
@@ -1,3 +1,13 @@
 echo "Ejecutando script desde repo externo"
 echo "Variable VERSION: $VAR1"
 echo "Variable ENVIRONMENT: $VAR2"
+# repo-scripts/scripts/mi-script.sh
+#!/bin/bash
+
+# Leer las variables pasadas como argumentos o desde entorno
+MAIN_VAR1=${1:-$VAR1}
+MAIN_VAR2=${2:-$VAR2}
+
+echo "Ejecutando script desde otro repo"
+echo "Variable 1 recibida: $VAR1"
+echo "Variable 2 recibida: $VAR2"
